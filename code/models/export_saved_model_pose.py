2af53ca2971a59b157aef956761e26bcc9618711
hexicle <hexicle@users.noreply.github.com>
2019-04-12 17:39:26 +0000

    added classifier models generated for Tensorflow Serving

---
+++ code/models/export_saved_model_pose.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+def export_saved_model(h5_model_input_path, pb_model_output_path):
+    """
+    Exports a h5 model (keras format) into pb model (tensorflow format)
+    so that Tensorflow Serving can serve it.
+    """
+    # The export path contains the name and the version of the model
+    tf.keras.backend.set_learning_phase(0)  # Ignore dropout at inference
+    model = tf.keras.models.load_model(h5_model_input_path)
+    export_path = pb_model_output_path
+
+    # Fetch the Keras session and save the model
+    # The signature definition is defined by the input and output tensors
+    # And stored with the default serving key
+    with tf.keras.backend.get_session() as sess:
+        tf.saved_model.simple_save(
+            sess,
+            export_path,
+            inputs={'input_image': model.input},
+            outputs={t.name: t for t in model.outputs})
+
+
+export_saved_model(h5_model_input_path='./model_pose.h5',
+        pb_model_output_path='./my_pose_classifier/1')
